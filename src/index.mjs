6d537e42ae0798bf662c7c96c958419d670d7f9c
saya8811 <110326649+saya8811@users.noreply.github.com>
2023-10-08 10:45:59 +0000

    Update 2 files

--- src/index.mjs
+++ src/index.mjs
@@ -1,12 +1,23 @@
-import "./styles.css";
+// const,letなどの変数宣言
+var var1 = "変数";
+console.log(var1);
 
-console.log("a");
+var1 = "上書き";
+console.log(var1);
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// const オブジェクトで定義したプロパティ
+var var1 = "書き換え変数宣言";
+console.log(var1);
+
+const sa1 = {
+  name: "sa",
+  test: "test",
+};
+sa1.adres = "tok";
+sa1.test = "lesson";
+console.log(sa1);
+
+// constで定義したものは、追加可能
+const sa2 = ["ya", "mi", "ma"];
+puts.sa2("a");
+console.log(sa2);
